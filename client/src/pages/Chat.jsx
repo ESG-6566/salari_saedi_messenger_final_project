3c5210d6f3fb64aecbe940f71f325872663e7fc6
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2023-03-11 21:08:26 +0330

    تا دقیقه 26:28

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,11 +1,5 @@
 import React from "react"
 
-function Chat(){
-    returnn(
-        <dice>Chat</dice>
-    )
-}
-
 export default function Chat(){
     return <div>Chat</div>
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,52 @@
 import React from "react"
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+
+const handleSubmit = (event)=>{
+    event.preventDefault();
+    alert("form")
+}
+const handleChange = (event) => {};
 
 export default function Register(){
-    return <div>Register</div>
+    return <>
+    <FormContainer>
+        <form onSubmit={(event)=>handleSubmit(event)}>
+            <div className="nrand">
+                <img src="" alt=""/>
+                <h1>snappy</h1>
+            </div>
+            <input 
+            type="text" 
+            placeholder="Username" 
+            name="username" 
+            onChange={e=>handleChange(e)}
+            />
+            <input 
+            type="email" 
+            placeholder="Email" 
+            name="email" 
+            onChange={e=>handleChange(e)}
+            />
+            <input 
+            type="password" 
+            placeholder="Password" 
+            name="password" 
+            onChange={e=>handleChange(e)}
+            />
+            <input 
+            type="password" 
+            placeholder="Confirm Password" 
+            name="confirmPassword" 
+            onChange={e=>handleChange(e)}
+            />
+            <button type="submit">Create User</button>
+            <span>Already have an account ? 
+                <Link to="./login">Login</Link>
+            </span>
+        </form>
+    </FormContainer>
+    </>
 }
+
+const FormContainer = styled.div``
