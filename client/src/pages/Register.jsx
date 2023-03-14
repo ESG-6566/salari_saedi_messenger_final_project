cedd9a93f3c3c8cb75442ab9458c8f1de0fa0664
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2023-03-15 03:10:54 +0330

    edit register page template

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 import styled from "styled-components"
 import { Link } from "react-router-dom"
+// import Logo from "../assets/logo.svg"
+import Logo2 from "../assets/logo2.svg"
+import styles from '../mystyle.module.css'
 
 const handleSubmit = (event)=>{
     event.preventDefault();
@@ -8,12 +11,13 @@ const handleSubmit = (event)=>{
 }
 const handleChange = (event) => {};
 
+
 export default function Register(){
     return <>
     <FormContainer>
         <form onSubmit={(event)=>handleSubmit(event)}>
             <div className="nrand">
-                <img src="" alt=""/>
+                <img src={Logo2} alt="Logo"/>
                 <h1>snappy</h1>
             </div>
             <input 
@@ -49,4 +53,71 @@ export default function Register(){
     </>
 }
 
-const FormContainer = styled.div``
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #E0E0E0;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
+`;
